c103252cb7d7ae44039d026c043a6fc009af7406
Basheer Thiabat <bcheer@yahoo.com>
2025-10-31 18:02:01 +0300

    Add files via upload

---
+++ concurrency1_1/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+
+      //  Thread customThread = Thread.currentThread();
+
+        CustomThread customThread = new CustomThread();
+        customThread.start();
+
+        Runnable myRunnable = ()->{
+            for(int i = 1; i <= 8; i++){
+                System.out.print(" 2 ");
+                try{
+                    TimeUnit.SECONDS.sleep(1);
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Thread myThread = new Thread(myRunnable);
+        myThread.start();
+
+        for(int i = 1; i <=3; i++){
+            System.out.print(" 3 ");
+            try{
+                Thread.sleep(250);
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
